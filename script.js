7e768ab283b4da19cc85b19256e24e088fd58f41
sameer <sameer.ghawana@gmail.com>
2021-09-20 14:09:29 +0530

    basic functionality

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
 const Game = () => {
     let gameBoard = new Array(3);
     for(let i =0;i<3;i++){
         gameBoard[i] = new Array(3);
     }
-
 
     const clearBoard = () => {
         for(let i=0;i<3;i++){
@@ -11,11 +16,13 @@ const Game = () => {
                 gameBoard[i][j] = "";
             }
         }
+        displayContents();
     }
 
     const boardPlay = (i,j,value) => {
         if (!gameBoard[i][j]){
             gameBoard[i][j] = value;
+            displayContents();
         }
     }
 
@@ -23,7 +30,54 @@ const Game = () => {
         console.log(gameBoard);
     }
 
-    return { displayBoard, clearBoard, boardPlay }
-}
 
-const
+    return { gameBoard, displayBoard, clearBoard, boardPlay }
+}
+let newGame = Game();
+
+
+function displayContents() {
+    let gridContainer = document.querySelector('#gridContainer')
+    removeAllChildNodes(gridContainer);
+    for (let i =0;i<3;i++){
+        for (let j=0;j<3;j++){
+            let newdiv = document.createElement('div');
+            newdiv.classList.add('gridSquares');
+            newdiv.dataset.rowNumber = `${i}`;
+            newdiv.dataset.columnNumber = `${j}`
+            gridContainer.appendChild(newdiv);
+            newdiv.textContent = newGame.gameBoard[i][j];
+        }
+    }
+}
+displayContents();
+
+
+const Player = (name,symbol) => {
+    let gridSquares = document.querySelectorAll('.gridSquares');
+    
+    if (symbol === 'X') {
+        gridSquares.forEach(grid => {
+            grid.addEventListener('click',e => {
+                newGame.boardPlay(grid.dataset.rowNumber,grid.dataset.columnNumber,'X');
+            })
+        })
+    } else if (symbol === "O") {
+        gridSquares.forEach(grid => {
+            grid.addEventListener('click',e => {
+                newGame.boardPlay(grid.dataset.rowNumber,grid.dataset.columnNumber,'O');
+            })
+        })
+    }
+    
+    return {name}
+}
+let player1 = Player('Sam','X');
+let player2 = Player('Bot','O');
+
+let clearButton = document.getElementById('clear');
+clearButton.addEventListener('click',e=> {
+    newGame.clearBoard();
+})
+
+
